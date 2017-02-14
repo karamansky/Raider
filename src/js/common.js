07ab066bb76b5d5b95b7e39a778135775b693672
karamansky <karamansky@gmail.com>
2017-02-14 10:27:16 +0200

    index - 90%-responsive

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,4 @@
 "use strict";
-
 $(function(){
 
 	$(window).on("load",function(){
@@ -8,7 +7,34 @@ $(function(){
 		});
 	});
 
+/*popup button, popup form*/
+	$(".popup").magnificPopup();
+	$(document).on('click', '.close_popup', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+/*end popup button, popup form*/
 
+
+/*owl-carousel configurations*/
+	$(".slider").owlCarousel({
+		items					: 1,
+		loop					:true,
+		navigation		:true,
+		navText 			: ["",""]
+	});
+	var slider = $(".slider").data('owlCarousel');
+	$(".prev").click(function(){
+		slider.prev();
+	});
+	$(".next").click(function(){
+		slider.next();
+	});
+/*end owl-carousel configurations*/
+
+
+
+/*show / hide description*/
 	$(".description__info").on("click", function(){
 		event.preventDefault();
 		var id = $(this).attr("href");
@@ -16,11 +42,9 @@ $(function(){
 	});
 	$(".close_description").on("click", function(){
 		event.preventDefault();
-
 		$(this).parent().hide();
-
 	});
-
+/*show / hide description*/
 
 
 });
